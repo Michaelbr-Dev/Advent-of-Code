7304fa1b32b6dd2748bc3a112a2588d8e83dabee
Michael Briquet <michael.br@free.fr>
2022-12-01 23:50:01 +0100

    ✨ Add 2022-12-01 Advent of Code contribution

---
+++ 2022/01/puzzle_one.js
@@ -0,0 +1,16 @@
+/**
+ * @file Puzzle one.
+ * @author Michael Briquet <contact@michaelbr-dev.fr>
+ */
+
+const fs = require('fs/promises');
+
+fs.readFile('./input.txt', 'utf8').then((data) => {
+  const [solution] = data
+    .split('\n\n')
+    // eslint-disable-next-line no-shadow
+    .map((n) => n.split('\n').reduce((p, n) => p + +n, 0))
+    .sort((a, b) => b - a);
+  // eslint-disable-next-line no-console
+  console.log(solution);
+});
