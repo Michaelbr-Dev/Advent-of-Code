033712f172a2913c0930517631b17ed83a7deb52
Michael Briquet <michael.br@free.fr>
2022-12-02 11:39:29 +0100

    ♻️ Code refactorisation

--- 2022/01/puzzle_one.js
+++ 2022/01/puzzle_one.js
@@ -8,8 +8,7 @@ const fs = require('fs/promises');
 fs.readFile('./input.txt', 'utf8').then((data) => {
   const [solution] = data
     .split('\n\n')
-    // eslint-disable-next-line no-shadow
-    .map((n) => n.split('\n').reduce((p, n) => p + +n, 0))
+    .map((n) => n.split('\n').reduce((a, b) => a + +b, 0))
     .sort((a, b) => b - a);
   // eslint-disable-next-line no-console
   console.log(solution);
